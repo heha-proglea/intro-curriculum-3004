4ce292232891e5a39bb67c27536641d106111666
heha-proglea <heha965+proglea@gmail.com>
2018-10-19 02:05:04 +0000

    update 解説コメントの追加

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// 整数の配列を受け取り、全て足し合わせる
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +8,8 @@ function add(numbers) {
     }
     return result;
 }
+
+// npmパッケージにaddメソッドを追加(=関数をモジュール化した)(←module.exportsオブジェクトのプロパティとして関数を登録してる)
 module.exports = {
     add: add
 };
